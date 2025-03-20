245ed9bd2a24fde7a87fc0e640968c60d7756fe3
Tomas Wallick <wallick.tom@gmail.com>
2025-03-20 13:49:15 +0100

    updated

--- crossplane-control-plane-cluster/crossplane-control-plane-kind-cluster-gcp-identity.sh
+++ crossplane-control-plane-cluster/crossplane-control-plane-kind-cluster-gcp-identity.sh
@@ -6,7 +6,7 @@ set -u  # Treat unset variables as an error.
 
 echo "Setting Up GCP IAM Workload Identity for KinD Crossplane Control Plane..."
 
-### Configure (Set) Required Environment Variables
+### Configure Required Variables
 export GCP_PROJECT_ID="cxp-gcp"
 export GCP_REGION="us-west4"
 
@@ -21,12 +21,12 @@ export KIND_K8S_SERVICE_ACCOUNT="crossplane-sa"
 
 echo "Environment Variables Configured."
 
-### (2) Authenticate with GCP & Set Project
+### (1) Authenticate & Set Project
 gcloud auth login --quiet
 gcloud config set project $GCP_PROJECT_ID
 echo "GCP authentication complete."
 
-### (3) Enable Required GCP APIs
+### (2) Enable Required GCP APIs
 echo "Enabling GCP APIs..."
 gcloud services enable \
     iam.googleapis.com \
@@ -47,20 +47,42 @@ gcloud services enable \
 
 echo "Required GCP APIs enabled."
 
-### (4) Create GCP IAM Service Account
+### (3) Create IAM Service Account
 echo "Creating GCP IAM Service Account..."
 gcloud iam service-accounts create $GCP_IAM_SERVICE_ACCOUNT \
   --description="IAM Service Account for KinD Crossplane Workload Identity" \
-  --display-name="KinD-CXP Workload ID"
+  --display-name="KinD-CXP Workload ID" || true
 
 echo "IAM Service Account Created: $GCP_IAM_SERVICE_ACCOUNT"
+
+### (4) Assign Permissions to IAM Service Account
+echo "Assigning IAM Roles to Service Account..."
+gcloud projects add-iam-policy-binding $GCP_PROJECT_ID \
+  --member="serviceAccount:$GCP_IAM_SERVICE_ACCOUNT_EMAIL" \
+  --role="roles/iam.workloadIdentityUser" --quiet || true
+
+gcloud projects add-iam-policy-binding $GCP_PROJECT_ID \
+  --member="serviceAccount:$GCP_IAM_SERVICE_ACCOUNT_EMAIL" \
+  --role="roles/secretmanager.secretAccessor" --quiet || true
+
+gcloud projects add-iam-policy-binding $GCP_PROJECT_ID \
+  --member="serviceAccount:$GCP_IAM_SERVICE_ACCOUNT_EMAIL" \
+  --role="roles/resourcemanager.projectIamAdmin" --quiet || true
+
+# NEW: Allow the user to impersonate this service account!
+gcloud iam service-accounts add-iam-policy-binding $GCP_IAM_SERVICE_ACCOUNT_EMAIL \
+  --member="user:wallick.tom@gmail.com" \
+  --role="roles/iam.serviceAccountTokenCreator" \
+  --project=$GCP_PROJECT_ID --quiet || true
+
+echo "IAM Roles Assigned."
 
 ### (5) Configure Workload Identity Federation
 echo "Configuring Workload Identity Federation..."
 gcloud iam workload-identity-pools create $WORKLOAD_IDENTITY_POOL \
   --project=$GCP_PROJECT_ID \
   --location="global" \
-  --display-name="KinD-CXP WI Pool"
+  --display-name="KinD-CXP Workload ID Pool" || true
 
 gcloud iam workload-identity-pools providers create-oidc $WORKLOAD_IDENTITY_PROVIDER \
   --project=$GCP_PROJECT_ID \
@@ -68,44 +90,20 @@ gcloud iam workload-identity-pools providers create-oidc $WORKLOAD_IDENTITY_PROV
   --workload-identity-pool=$WORKLOAD_IDENTITY_POOL \
   --display-name="KinD-CXP OIDC Provider" \
   --attribute-mapping="google.subject=assertion.sub" \
-  --issuer-uri="https://container.googleapis.com/v1/projects/${GCP_PROJECT_ID}/locations/${GCP_REGION}/clusters/kind-crossplane-cluster"
+  --issuer-uri="https://container.googleapis.com/v1/projects/${GCP_PROJECT_ID}/locations/${GCP_REGION}/clusters/kind-crossplane-cluster" || true
 
 echo "Workload Identity Federation Configured."
 
-# **Wait for Workload Identity Pool to be fully registered in GCP**
-echo "Waiting for Workload Identity Pool to propagate..."
-sleep 45
-
-# **Verify that the identity pool now exists**
-echo "Verifying Workload Identity Pool..."
-gcloud iam workload-identity-pools list --location="global"
-
-### (6) Grant Required IAM Permissions
-echo "Granting IAM Roles to Service Account..."
-gcloud projects add-iam-policy-binding $GCP_PROJECT_ID \
-  --member="serviceAccount:$GCP_IAM_SERVICE_ACCOUNT_EMAIL" \
-  --role="roles/iam.workloadIdentityUser"
-
-gcloud projects add-iam-policy-binding $GCP_PROJECT_ID \
-  --member="serviceAccount:$GCP_IAM_SERVICE_ACCOUNT_EMAIL" \
-  --role="roles/secretmanager.secretAccessor"
-
-gcloud projects add-iam-policy-binding $GCP_PROJECT_ID \
-  --member="serviceAccount:$GCP_IAM_SERVICE_ACCOUNT_EMAIL" \
-  --role="roles/resourcemanager.projectIamAdmin"
-
-echo "IAM Roles Assigned."
-
-### (7) Associate KinD Kubernetes Service Account with Workload Identity
+### (6) Associate KinD Kubernetes Service Account with Workload Identity
 echo "Associating KinD Kubernetes Service Account with Workload Identity..."
 gcloud iam service-accounts add-iam-policy-binding $GCP_IAM_SERVICE_ACCOUNT_EMAIL \
   --project=$GCP_PROJECT_ID \
   --role=roles/iam.workloadIdentityUser \
-  --member="principalSet://iam.googleapis.com/projects/$GCP_PROJECT_ID/locations/global/workloadIdentityPools/$WORKLOAD_IDENTITY_POOL/attribute.google.subject/${KIND_K8S_SERVICE_ACCOUNT}"
+  --member="principalSet://iam.googleapis.com/projects/$GCP_PROJECT_ID/locations/global/workloadIdentityPools/$WORKLOAD_IDENTITY_POOL/attribute.google.subject/${KIND_K8S_SERVICE_ACCOUNT}" --quiet || true
 
 kubectl annotate serviceaccount \
   --namespace $KIND_K8S_NAMESPACE $KIND_K8S_SERVICE_ACCOUNT \
-  iam.gke.io/gcp-service-account=$GCP_IAM_SERVICE_ACCOUNT_EMAIL
+  iam.gke.io/gcp-service-account=$GCP_IAM_SERVICE_ACCOUNT_EMAIL --overwrite || true
 
 echo "KinD Kubernetes Service Account is now linked to Workload Identity!"
 
