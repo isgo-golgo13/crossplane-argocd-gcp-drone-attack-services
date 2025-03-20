1253a7ff02b2c56f4aad8ef2975b087012d26385
Tomas Wallick <wallick.tom@gmail.com>
2025-03-20 13:27:09 +0100

    updated

--- crossplane-control-plane-cluster/crossplane-control-plane-kind-cluster-gcp-identity-delete-check.sh
+++ crossplane-control-plane-cluster/crossplane-control-plane-kind-cluster-gcp-identity-delete-check.sh
@@ -15,15 +15,15 @@ GCP_IAM_SERVICE_ACCOUNT_EMAIL="${GCP_IAM_SERVICE_ACCOUNT}@${GCP_PROJECT_ID}.iam.
 KIND_K8S_NAMESPACE="crossplane-system"
 
 echo "Checking Workload Identity Pool..."
-gcloud iam workload-identity-pools list --location="global" --format="table(name, state)"
+gcloud iam workload-identity-pools list --location="global" --format="table(name, state)" || echo "No Workload Identity Pools found."
 
 echo "Checking Workload Identity Provider..."
-gcloud iam workload-identity-pools providers list --location="global" --format="table(name, state)"
+gcloud iam workload-identity-pools providers list --workload-identity-pool="${WORKLOAD_IDENTITY_POOL}" --location="global" --format="table(name, state)" || echo "No Workload Identity Providers found."
 
 echo "Checking IAM Service Account..."
-gcloud iam service-accounts list --filter="email:${GCP_IAM_SERVICE_ACCOUNT_EMAIL}" --format="table(email, disabled)"
+gcloud iam service-accounts list --filter="email:${GCP_IAM_SERVICE_ACCOUNT_EMAIL}" --format="table(email, disabled)" || echo "IAM Service Account not found."
 
 echo "Checking Kubernetes Namespace..."
 kubectl get ns ${KIND_K8S_NAMESPACE} || echo "Namespace ${KIND_K8S_NAMESPACE} not found."
 
-echo "Verification complete. If no resources are listed , everything is clean."
+echo "Verification complete. If no resources are listed, everything is clean."
